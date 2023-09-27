9f7a17f9a912bd3b2924f7ffb79209b88e7af9e3
sergioleomar <sergio.dias2@fatec.sp.gov.br>
2023-09-27 09:12:45 -0300

    Adicionando dockerfile para expor as APIs - Adicionando dependency servlet-api

--- src/main/java/com/example/demointegracaomysqlsalesforce/security/WebConfigSecurity.java
+++ src/main/java/com/example/demointegracaomysqlsalesforce/security/WebConfigSecurity.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
 	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override
